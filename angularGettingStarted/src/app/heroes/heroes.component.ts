581f3ab5ef8bd22093b2176920dce1b42b057f16
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-02-18 21:19:28 +0100

    simple dependency injection for heroes

--- angularGettingStarted/src/app/heroes/heroes.component.ts
+++ angularGettingStarted/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -21,11 +21,16 @@ export class HeroesComponent implements OnInit {
   }
   
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
